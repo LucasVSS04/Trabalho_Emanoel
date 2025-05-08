8f5da07ac4b2a380ba66fa79837bb40634908a62
Gabriel Couzzi <130262415+GCouzzi@users.noreply.github.com>
2025-05-08 20:11:54 -0300

    Removendo arquivos

--- src/routes/usuario.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const UsuarioController = require('../controllers/UsuarioController');
-
-// Rota para obter informações do usuário
-router.get('/info', UsuarioController.info);
-
-module.exports = router; 
